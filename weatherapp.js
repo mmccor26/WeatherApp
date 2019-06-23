e7fdf0cf96683ea0e17774197644d183d9145514
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-06-23 09:57:17 -0400

    Code Refactoring

--- weatherapp.js
+++ weatherapp.js
@@ -4,35 +4,31 @@ let temp;
 let weatherDescription;
 let maxTemp = document.getElementById("tempMax");
 let description = document.getElementById("description");
+let city = document.getElementById("city");
+
+GetWeatherData("Paris");
 searchbtn.addEventListener('click',()=>{
     let location = `${searchBar.value}`;
-    console.log(location);
+    
+    GetWeatherData(location);
+    
+});
+
+async function GetWeatherData(location){
     weather = fetch('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=a0a56053415cd2354338eb10e89d0fd6',{mode: 'cors'})
     .then(function(response){
         return response.json()
     })
     .then(function(response){
-        console.log(response);
-        console.log(response.main.temp_max)
-        console.log(response.weather[0].description)
+
+        city.innerHTML = location;
         temp = response.main.temp_max;
         weatherDescription = response.weather[0].description;
-        maxTemp.innerHTML = temp;
+        maxTemp.innerHTML = Math.round(temp - 273.15);
         description.innerHTML = weatherDescription;
         
     })
-    
-});
-
-async function GetWeatherData(location){
-        let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=a0a56053415cd2354338eb10e89d0fd6',{mode: 'cors'});
-    
-        let data = await response.json()
-        
-        return data;
+       
     
 }
 
-function ProcessJSON(json){
-    
-}
